ea9b22f040b7ed79d0974a3f8dbb6618daa0778c
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-09-20 17:04:12 +0800

    upadd

--- login.php
+++ login.php
@@ -51,140 +51,39 @@ function myTimer() {
 
 
     //sql statement
+    $sql = "SELECT * FROM users WHERE username='$username' and password='$password'";
+ 
+    //Db Connection
+    require_once('db.php');
+
+    //qry
+    $qry = mysqli_query ($conn, $sql) or die ("Login problem");
+    $count = mysqli_num_rows($qry);
+    if($count==1)
+    {
+      $_SESSION['username']= $username;
+      //header("location: dashboard.php");
+      header("location: index.php");
+      
+    }
+
+    if($count<>1);
+    {
+      $_SESSION['username']= $username;
+      //header("location: dashboard.php");
+    //  header("location: index.php");
+  echo"<script>alert('Error=Incorrect User Name or password.')</script>";
+   
+
+
+    }
+
+
+
+
+
+    //sql statement
     $sql = "SELECT * FROM users_tbl WHERE username='$username' and password='$password'";
- 
-    //Db Connection
-    require_once('db.php');
-
-    //qry
-    $qry = mysqli_query ($conn, $sql) or die ("Login problem");
-    $count = mysqli_num_rows($qry);
-    if($count==1)
-    {
-      $_SESSION['username']= $username;
-      //header("location: dashboard.php");
-      // header("location: index.php");
-      
-    }
-
-    if($count===1);
-    {
-      $_SESSION['username']= $username;
-      //header("location: dashboard.php");
-    //  header("location: index.php");
-  echo"<script>alert('Error=Incorrect User Name or password.')</script>";
-   
-
-
-    }
-
-
-
-
-
-    //sql statement
-    $sql = "SELECT * FROM user_tbl WHERE username='$username' and password='$password'";
- 
-    //Db Connection
-    require_once('db.php');
-
-    //qry
-    $qry = mysqli_query ($conn, $sql) or die ("Login problem");
-    $count = mysqli_num_rows($qry);
-    if($count==1)
-    {
-      $row=mysqli_fetch_assoc($qry);
-
-      $_SESSION['id']= $row['id'];
-      $_SESSION['username']= $row['username'];
-      $_SESSION['email']= $row['email'];
-      $_SESSION['password']= $row['password'];
-      $_SESSION['status']= $row['status'];
-      $_SESSION['role']= $row['role'];
-      $_SESSION['department_id']= $row['department_id'];
-
-      if ($_SESSION['department_id'] == 1) {
-        header("location: indexasds.php");
-      }
-      elseif ($_SESSION['department_id'] == 2) {
-        header("location: indexsds.php");
-      }
-
-      elseif ($_SESSION['department_id'] == 3) {
-        header("location: indexrecord.php");
-      }
-
-      elseif ($_SESSION['department_id'] == 4) {
-        header("location: indexqueue.php");
-      }
-
-      elseif ($_SESSION['department_id'] == 5) {
-        header("location: indexhrmo.php");
-      }
-
-      elseif ($_SESSION['department_id'] == 6) {
-        header("location: indexsgod.php");
-      }
-      elseif ($_SESSION['department_id'] == 7) {
-        header("location: indexcid.php");
-      }
-
-      elseif ($_SESSION['department_id'] == 8) {
-        header("location: indexacct.php");
-      }
-
-      elseif ($_SESSION['department_id'] == 10) {
-        header("location: indexsupply.php");
-      }
-
-      elseif ($_SESSION['department_id'] == 11) {
-        header("location: indexbudget.php");
-      }
-
-      elseif ($_SESSION['department_id'] == 12) {
-        header("location: indexcashier.php");
-      }
-
-      elseif ($_SESSION['department_id'] == 13) {
-        header("location: indexlegal.php");
-      }
-
-      elseif ($_SESSION['department_id'] == 14) {
-        header("location: indexdpsu.php");
-      }
-
-      elseif ($_SESSION['department_id'] == 15) {
-        header("location: index_trans.php");
-      }
-
-      elseif ($_SESSION['department_id'] == 16) {
-        header("location: index.php");
-      }
-
-
-
-      //header("location: dashboard.php");
-      // header("location: index.php");
-      
-    }
-
-    if($count===1);
-    {
-      $_SESSION['username']= $username;
-      //header("location: dashboard.php");
-    //  header("location: index.php");
-  echo"<script>alert('Error=Incorrect User Name or password.')</script>";
-   
-
-
-    }
-
-
-
-
-
-    //sql statement
-    $sql = "SELECT * FROM user_tbl WHERE username='$username' and password='$password'";
  
     //Db Connection
     require_once('db.php');
