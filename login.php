288e1106754478d0571f47d49b86caa643c270f9
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-10-04 21:51:03 +0800

    Update login.php

--- login.php
+++ login.php
@@ -323,15 +323,6 @@ function myTimer() {
 
 
 
-<!--header-->
-
-<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
-
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- Add icon library -->
