79bc4fddcda7607d17786e7800da0204d34476d4
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-10-05 19:20:41 +0800

    Update login.php

--- login.php
+++ login.php
@@ -526,7 +526,7 @@ body {
 
     <li> <a href="#"><font color="White" size="2"><B><?=$_SESSION['username']?></B></font></a>
      
-    <a href="http://202.137.126.58"><font color="White" size="2"><B>   |    Home</B></font></a> <li> 
+    <a href="http://202.137.126.58/"><font color="White" size="2"><B>   |    Home</B></font></a> <li> 
       </ul>
 </div>
   
