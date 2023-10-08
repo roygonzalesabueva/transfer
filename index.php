b83ba7cc0470e655a3e67bc5b0157eb1b47b0584
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-10-08 08:38:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -505,14 +505,11 @@ function myTimer() {
 <!-- </center> -->
 		
 
-<div class="col-md-3"></div>
 	<!--  <div class="col-md-20 well">-->
 
    <center><button class="btn btn-primary" onclick="generateTrackingID()" type="button" data-toggle="modal" data-target="#form_modal"> <font size="3"> <B>Create Transaction</B></font></button></center> 
 
 	
-		<hr style="border-top:1px dotted #ccc;"/>
-		
 
     <!-- <center>
         <font size="5">
