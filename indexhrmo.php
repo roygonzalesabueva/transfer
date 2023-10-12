4fd5361d1e1360d615f8702cc08c7041eb36d703
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-10-13 00:03:02 +0800

    bb

--- indexhrmo.php
+++ indexhrmo.php
@@ -247,6 +247,7 @@ body {
     </ul>-->
     <ul class="nav navbar-nav navbar-right">
     <li> <a href="#"><font color="White" size="4"><B> <p id="demo"></p> </B></font></a> </li>
+    <li> <a href="chathrmo.php"><font color="White" size="4"><B>Chat Room</B></font></a></li>
 		
     <li>  
     <a href="indexclient_hrmo.php"> <i class="fa fa-bar-chart" style="font-size:18px"></i><font color="White" size="4"><B>Transaction Status</B></font></a>
