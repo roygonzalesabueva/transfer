6646b7a1d96ec8460c471421f275329ca06ff1f4
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-10-13 00:21:16 +0800

    ff

--- indexlegal.php
+++ indexlegal.php
@@ -220,7 +220,7 @@ body {
     </ul>-->
     <ul class="nav navbar-nav navbar-right">
     <li> <a href="#"><font color="White" size="4"><B> <p id="demo"></p> </B></font></a> </li>
-		
+	<li> <a href="chatlegal.php"><font color="White" size="4"><B>Chat Room</B></font></a></li>	
     <li>  
     <a href="indexclient_legal.php"> <i class="fa fa-bar-chart" style="font-size:18px"></i><font color="White" size="4"><B>Transaction Status</B></font></a>
           </li>
