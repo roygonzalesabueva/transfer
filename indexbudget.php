9f19fb811b9298e60a516056b53cf288be3dc762
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-10-13 00:36:06 +0800

    bb

--- indexbudget.php
+++ indexbudget.php
@@ -248,7 +248,7 @@ div.c {
   
     <ul class="nav navbar-nav navbar-right">
     <li> <a href="#"><font color="White" size="4"><B> <p id="demo"></p> </B></font></a> </li>
-		
+    <li> <a href="chatbudget.php"><font color="White" size="4"><B>Chat Room</B></font></a></li>
     <li>  
     <a href="indexclient_budget.php"> <i class="fa fa-bar-chart" style="font-size:18px"></i><font color="White" size="4"><B>Transaction Status</B></font></a>
           </li>
