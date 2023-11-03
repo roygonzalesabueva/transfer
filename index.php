eeaa09383f7304e7696e1133882c29278cfc7431
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-03 22:26:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -432,7 +432,7 @@ function myTimer() {
   
   
              
-            <li>  <a href="#"><div class="dropdown"> <font color="BLUE" size="4">	  SECTIONS  </font></a>
+            <li>  <a href="#"><div class="dropdown">  SECTIONS  <font color="BLUE" size="4">	 </font></a>
                 
 					
 							<div class="dropdown-content">
