ac42f1bcaa087338bc7667159b5012269069a254
Geperson Mamalias <kuyagepofficial@gmail.com>
2023-12-29 22:37:35 +0800

    Update login.php

--- login.php
+++ login.php
@@ -13,6 +13,7 @@ if (!isset($_SESSION['username'])) {
 }
 ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <!-- <script>
   setInterval(myTimer, 1000);
 
