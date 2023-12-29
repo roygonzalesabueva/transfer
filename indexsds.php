0a2cee9c67c74c749c034baea6464c488e9a96f7
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-12-29 21:45:21 +0800

    Update indexsds.php

--- indexsds.php
+++ indexsds.php
@@ -299,7 +299,7 @@ body {
     <a href="indexclient_sds.php"> <i class="fa fa-bar-chart" style="font-size:18px"></i><font color="White" size="4"><B>Transaction Status</B></font></a>
           </li>
         
-        <li><a href="#"> <font color="White" size="4"><B><i class="fa fa-fw fa-user"></i>   <span class="nav-profile-name"><?php echo $_SESSION['user'];?> </span></B></font></a></li>
+        <li><a href="#"> <font color="White" size="4"><B><i class="fa fa-fw fa-user"></i>   <span class="nav-profile-name"><?php echo $_SESSION['username'];?> </span></B></font></a></li>
       
      
       <li> <a href="http://202.137.126.58"><font color="White" size="4"><B>Home</B></font></a></li>
@@ -389,7 +389,7 @@ body {
 					
 					<td><?php echo $fetch['trackid']?></td>
 					
-						<td><?php echo $_SESSION['user'];?></td>
+						<td><?php echo $_SESSION['username'];?></td>
 						<td><?php echo $fetch['lastname']?></td>
 						
 						
