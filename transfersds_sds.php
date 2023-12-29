9c09618c964e3480d81a96ebf1c1ae6c9642dcb7
Geperson Mamalias <kuyagepofficial@gmail.com>
2023-12-29 21:45:20 +0800

    initial commit

--- transfersds_sds.php
+++ transfersds_sds.php
@@ -14,10 +14,11 @@ if (isset($_REQUEST['mem_id'])) {
 	$address = $fetch['address'];
 
 	//mysqli_query($conn, "INSERT INTO `membertracking` VALUES('','$trackid','$firstname', '$lastname', '', '$address')") or die(mysqli_error());
-	mysqli_query($conn, "INSERT INTO `membersds` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
-	mysqli_query($conn, "INSERT INTO `memberclient` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
+	mysqli_query($conn, "INSERT INTO `membersds` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die("DB Connection Error");
+	mysqli_query($conn, "INSERT INTO `memberclient` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die("DB Connection Error");
 	//mysqli_query($conn, "INSERT INTO `membertracking` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
-	mysqli_query($conn, "DELETE FROM `membersds` WHERE `mem_id`='$mem_id'") or die(mysqli_error());
+	mysqli_query($conn, "DELETE FROM `membersds` WHERE `mem_id`='$mem_id'") or
+	die("DB Connection Error");
 
 	//echo"<script>confirm('Data successfully transfer')</script>";
 	echo "<script>window.location='indexsds.php'</script>";
