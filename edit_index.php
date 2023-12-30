0f887fec19a1ade51d61d1ef0f7cd2c37bd5c7e5
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-12-30 21:22:35 +0800

    Update edit_index.php

--- edit_index.php
+++ edit_index.php
@@ -300,8 +300,8 @@ else{
 <i class="fa fa-user icon"></i>
 <select type="text" name="firstname" value="<?php  echo $firstname;    ?>">
  
-
-        <option value="Records">Records</option>
+<!-- <option type="text" value="<?php echo $_SESSION['username']; ?>"</option>   -->
+        <option value="<?php echo $_SESSION['username']; ?>"><?php echo $_SESSION['username']; ?></option>
         </select>
 </div>
 
