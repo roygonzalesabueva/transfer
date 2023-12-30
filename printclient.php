908769c29fe521a151a7f06b5865f82ae6fb7f38
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-12-30 23:39:53 +0800

    Update printclient.php

--- printclient.php
+++ printclient.php
@@ -135,11 +135,11 @@ while($rows=mysqli_fetch_array($query)){
     $y= $pdf->GetY();
     
 
-    $pdf->MultiCell(30, $h, $rows['firstname'], 1, 'C');
+    $pdf->MultiCell(30, $h, $rows['firstname'], 1, 0, 'C');
     $y1= $pdf->GetY();
     $pdf->SetY($y);
     $pdf->Cell(58,$h,'');
-    $pdf->MultiCell(45, $h, $rows['lastname'], 1, 'C');
+    $pdf->MultiCell(45, $h, $rows['lastname'], 1, 0, 'C');
     $y1= $pdf->GetY();
     $pdf->SetY($y);
     $pdf->Cell(103,$h,'');
