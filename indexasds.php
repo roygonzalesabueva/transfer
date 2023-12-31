365a4e402901f8e3bcca98ce75cb5ab28df451d7
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-12-31 12:36:48 +0800

    Update indexasds.php

--- indexasds.php
+++ indexasds.php
@@ -327,7 +327,7 @@ body {
 					
 					<td><?php echo $fetch['trackid']?></td>
 					
-						<td><?php echo $_SESSION['user'];?></td>
+                    <td><?php echo $fetch['firstname']?></td>
 						<td><?php echo $fetch['lastname']?></td>
 						
 						
