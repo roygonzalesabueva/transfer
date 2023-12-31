7c06b239ade444a024e23778e43c17f69b7792b8
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-12-31 15:55:29 +0800

    Update printclient2.php

--- printclient2.php
+++ printclient2.php
@@ -65,7 +65,7 @@ class MyPDF extends FPDF {
 function Footer()
 {
 	
-	$this->Image('STAMP.jpg',65,200,80,0);
+	$this->Image('STAMP.png',65,200,80,0);
 	
     $this->Image('sds.jpg',65,230,80,0);
 	
