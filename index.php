cf6c4d8f35911c409c58092046588a98b7698227
roygonzalesabueva <roy.abueva@deped.gov.ph>
2024-01-07 02:24:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1044,20 +1044,14 @@ function myTimer() {
             if (result.isConfirmed) {
                 window.location = ("acceptindex.php?mem_id=" + data_id);
               
-                 
-                
-            }
-
-        }).then((result) => {
-            if (result.isConfirmed) {
-              
-              
-                window.location = ("edit_index.php?mem_id=" + data_id);  
+              //  window.location = ("edit_index.php?mem_id=" + data_id);  
                 
             }
 
         })
 
+
+        
     }
     </script>
 
