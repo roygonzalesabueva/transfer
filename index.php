b768e9c4c3b7a6c3b2521389f9a74362b7723112
roygonzalesabueva <roy.abueva@deped.gov.ph>
2024-01-07 00:12:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -751,8 +751,8 @@ function myTimer() {
                                </i></a> -->
 
 
-                            <a onclick="acceptindex('<?php echo $fetch['mem_id']; ?>')" class=" btn btn-sm btn-success"
-                                href="edit_index.php?mem_id=<?php echo $fetch['mem_id'] ?>"><i
+                            <a  class=" btn btn-sm btn-success" onclick="acceptindex('<?php echo $fetch['mem_id']; ?>')"
+                                href="edit_index.php?mem_id=<?php echo $fetch['mem_id'] ?>"> <i
                                     class="fa fa-edit"></i>Update</a>
 
                         </td>
