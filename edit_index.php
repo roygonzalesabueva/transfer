f7c716f8908e6c751c257dba1c58022246493973
roygonzalesabueva <roy.abueva@deped.gov.ph>
2024-01-07 21:55:04 +0800

    vv

--- edit_index.php
+++ edit_index.php
@@ -231,42 +231,6 @@ body {
 
 
 
-<?php
-	require_once'conn.php';
-
-
-
-	
-	if(ISSET($_REQUEST['mem_id'])){
-		$mem_id=$_REQUEST['mem_id'];
-		
-		$query=mysqli_query($conn, "SELECT * FROM `member1` WHERE `mem_id`='$mem_id'") or die(mysqli_error());
-		$fetch=mysqli_fetch_array($query);
-		
-		$trackid=$fetch['trackid'];
-		$firstname=$fetch['firstname'];
-		$lastname=$fetch['lastname'];
-		$section=$fetch['section'];
-		$address=$fetch['address'];
-
-		
-		
-		//mysqli_query($conn, "INSERT INTO `membertracking` VALUES('','$trackid','$firstname', '$lastname', '', '$address')") or die(mysqli_error());
-	//mysqli_query($conn, "INSERT INTO `memberrecord` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
-		mysqli_query($conn, "INSERT INTO `memberclient` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
-		//mysqli_query($conn, "DELETE FROM `member1` WHERE `mem_id`='$mem_id'") or die(mysqli_error());
-		
-
-		echo"<script>alert('Transaction Accepted')</script>";
-		// echo"<script>alert('Transaction successfully deleted.')</script>";
-		//echo"<script>window.location='index.php'</script>";
-		//echo"<script>window.location='edit_index.php'</script>";
-		
-		
-	}
-?>
-
-
 
 
 
@@ -437,6 +401,66 @@ else{
 </div>
 </div>
 </div>
+
+
+
+
+
+
+
+
+
+<?php
+	require_once'conn.php';
+
+
+
+	
+	if(ISSET($_REQUEST['mem_id'])){
+		$mem_id=$_REQUEST['mem_id'];
+		
+		$query=mysqli_query($conn, "SELECT * FROM `member1` WHERE `mem_id`='$mem_id'") or die(mysqli_error());
+		$fetch=mysqli_fetch_array($query);
+		
+		$trackid=$fetch['trackid'];
+		$firstname=$fetch['firstname'];
+		$lastname=$fetch['lastname'];
+		$section=$fetch['section'];
+		$address=$fetch['address'];
+
+		
+		
+		//mysqli_query($conn, "INSERT INTO `membertracking` VALUES('','$trackid','$firstname', '$lastname', '', '$address')") or die(mysqli_error());
+	//mysqli_query($conn, "INSERT INTO `memberrecord` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
+		mysqli_query($conn, "INSERT INTO `memberclient` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
+		//mysqli_query($conn, "DELETE FROM `member1` WHERE `mem_id`='$mem_id'") or die(mysqli_error());
+		
+
+		echo"<script>alert('Transaction Accepted')</script>";
+		// echo"<script>alert('Transaction successfully deleted.')</script>";
+		//echo"<script>window.location='index.php'</script>";
+		//echo"<script>window.location='edit_index.php'</script>";
+		
+		
+	}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </body>
 
