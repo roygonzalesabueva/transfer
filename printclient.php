748de6206997faf2182fec17ab440e77d1d2a92b
roygonzalesabueva <roy.abueva@deped.gov.ph>
2024-01-13 12:42:26 +0800

    Update printclient.php

--- printclient.php
+++ printclient.php
@@ -147,7 +147,7 @@ while($rows=mysqli_fetch_array($query)){
     
     $y1= $pdf->GetY();
     $pdf->SetY($y);
-    $pdf->Cell(145,$h,'');
+    $pdf->Cell(135,$h,'');
 
     
     $pdf->Cell(35, $h, $rows['date_created'], 1, 0, 'C');
