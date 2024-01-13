bfb6e57f3fd152a2084768a7c9c4c7b6dba1eb67
roygonzalesabueva <roy.abueva@deped.gov.ph>
2024-01-14 06:12:30 +0800

    Update printclient.php

--- printclient.php
+++ printclient.php
@@ -132,10 +132,10 @@ $pdf->Cell(26, 8, 'Remarks', 1, 1, 'C');
 require'conn.php';
 
 $query=mysqli_query($conn, "SELECT * FROM `memberclient` WHERE trackid='{$_POST['trackid']}'  ") or die(mysqli_error());
-$h=5;
+$h=16;
 while($rows=mysqli_fetch_array($query)){
 
-    $pdf->Cell(50, $h, $rows['trackid'], 1, 0, 'C');
+    $pdf->Cell(28, $h, $rows['trackid'], 1, 0, 'C');
     $y= $pdf->GetY();
     
 
