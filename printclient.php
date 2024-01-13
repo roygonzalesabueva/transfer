f0b8d87df90327fd7b2ed73d22ada9dc0a1f8fa0
roygonzalesabueva <roy.abueva@deped.gov.ph>
2024-01-13 21:35:11 +0800

    Update printclient.php

--- printclient.php
+++ printclient.php
@@ -46,7 +46,7 @@ class MyPDF extends FPDF {
         $this->SetFont('Arial', 'B', 12);
         $this->Cell(0, 55, date("d-m-Y "), 0, 0, 'R');
 
-        $this->Cell(0,80,'TRANSACTIONS HISTORY',0,0,'C');
+        $this->Cell(195,60,'TRANSACTIONS HISTORY',0,0,'C');
         $this->Ln();
        
         // $this->SetFont('Times','B',16);
